67ee7abd7c489f6f428275d30f93848798431914
Selinadita <selinacahyani03@gmail.com>
2024-05-29 18:09:48 +0700

    upload coder project

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ function toggleDetail(e) {
 
     $(detail).slideToggle();
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("inp_email")
+    const subject = $("inp_subject")
+    const message = $("inp_message")
+
+    if (!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val()) {
+        alert("Subject is required")
+    } else if (!$(message).val()) {
+        alert("Massage is required")
+    } else {
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("form submitted")
+    }
+}
